64fba5e3386253b4b34deeaa8b09268691035b24
Hflati <147087305+Hflati@users.noreply.github.com>
2024-01-28 16:35:45 -0500

    Updated Chess Game

--- Project Code/Chess.py
+++ Project Code/Chess.py
@@ -14,38 +14,38 @@ fps = 60
 color = (64, 64, 64)
 
 #All Chess Pieces
-pieces = ['Rook', 'Knight', 'Bishop', 'King', 'Queen', 'Pawn']
+pieces = ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Pawn']
 
 #White Pieces
-whitePieces = ['Rook', 'Knight', 'Bishop', 'King', 'Queen', 'Bishop', 'Knight', 'Rook',
+whitePieces = ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook',
                'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn']
 
-whiteRook = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White Rook.png')
+whiteRook = pygame.image.load('..\Chess Pieces\White Pieces\White Rook.png')
 whiteRook = pygame.transform.scale(whiteRook, (80, 80))
 whiteRookCaptured = pygame.transform.scale(whiteRook, (45, 45))
 
-whiteKnight = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White Knight.png')
+whiteKnight = pygame.image.load('..\Chess Pieces\White Pieces\White Knight.png')
 whiteKnight = pygame.transform.scale(whiteKnight, (80, 80))
 whiteKnightCaptured = pygame.transform.scale(whiteKnight, (45, 45))
 
-whiteBishop = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White Bishop.png')
+whiteBishop = pygame.image.load('..\Chess Pieces\White Pieces\White Bishop.png')
 whiteBishop = pygame.transform.scale(whiteBishop, (80, 80))
 whiteBishopCaptured = pygame.transform.scale(whiteBishop, (45, 45))
 
-whiteKing = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White King.png')
+whiteKing = pygame.image.load('..\Chess Pieces\White Pieces\White King.png')
 whiteKing = pygame.transform.scale(whiteKing, (80, 80))
 whiteKingCaptured = pygame.transform.scale(whiteKing, (45, 45))
 
-whiteQueen = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White Queen.png')
+whiteQueen = pygame.image.load('..\Chess Pieces\White Pieces\White Queen.png')
 whiteQueen = pygame.transform.scale(whiteQueen, (80, 80))
 whiteQueenCaptured = pygame.transform.scale(whiteQueen, (45, 45))
 
-whitePawn = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\White Pieces\White Pawn.png')
+whitePawn = pygame.image.load('..\Chess Pieces\White Pieces\White Pawn.png')
 whitePawn = pygame.transform.scale(whitePawn, (60, 60))
 whitePawnCaptured = pygame.transform.scale(whitePawn, (45, 45))
 
-whitePictures = [whiteRook, whiteKnight, whiteBishop, whiteKing, whiteQueen, whitePawn]
-whitePicturesCaptured = [whiteRookCaptured, whiteKnightCaptured, whiteBishopCaptured, whiteKingCaptured, whiteQueenCaptured, whitePawnCaptured]
+whitePictures = [whiteRook, whiteKnight, whiteBishop, whiteQueen, whiteKing, whitePawn]
+whitePicturesCaptured = [whiteRookCaptured, whiteKnightCaptured, whiteBishopCaptured, whiteQueenCaptured, whiteKingCaptured, whitePawnCaptured]
 
 whitePiecesLocation = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
                        (0,6), (1,6), (2,6), (3,6), (4,6), (5,6), (6,6), (7,6)]
@@ -53,35 +53,35 @@ whitePiecesLocation = [(0,7), (1,7), (2,7), (3,7), (4,7), (5,7), (6,7), (7,7),
 whiteCaptured = []
 
 #Black Pieces
-blackPieces = ['Rook', 'Knight', 'Bishop', 'King', 'Queen', 'Bishop', 'Knight', 'Rook',
+blackPieces = ['Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Bishop', 'Knight', 'Rook',
                'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn', 'Pawn']
 
-blackRook = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black Rook.png')
+blackRook = pygame.image.load('..\Chess Pieces\Black Pieces\Black Rook.png')
 blackRook = pygame.transform.scale(blackRook, (80, 80))
 blackRookCaptured = pygame.transform.scale(blackRook, (45, 45))
 
-blackKnight = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black Knight.png')
+blackKnight = pygame.image.load('..\Chess Pieces\Black Pieces\Black Knight.png')
 blackKnight = pygame.transform.scale(blackKnight, (80, 80))
 blackKnightCaptured = pygame.transform.scale(blackKnight, (45, 45))
 
-blackBishop = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black Bishop.png')
+blackBishop = pygame.image.load('..\Chess Pieces\Black Pieces\Black Bishop.png')
 blackBishop = pygame.transform.scale(blackBishop, (80, 80))
 blackBishopCaptured = pygame.transform.scale(blackBishop, (45, 45))
 
-blackKing = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black King.png')
+blackKing = pygame.image.load('..\Chess Pieces\Black Pieces\Black King.png')
 blackKing = pygame.transform.scale(blackKing, (80, 80))
 blackKingCaptured = pygame.transform.scale(blackKing, (45, 45))
 
-blackQueen = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black Queen.png')
+blackQueen = pygame.image.load('..\Chess Pieces\Black Pieces\Black Queen.png')
 blackQueen = pygame.transform.scale(blackQueen, (80, 80))
 blackQueenCaptured = pygame.transform.scale(blackQueen, (45, 45))
 
-blackPawn = pygame.image.load('c:\YSU Classes\CSCI 4890 Computer Projects\Chess Pieces\Black Pieces\Black Pawn.png')
+blackPawn = pygame.image.load('..\Chess Pieces\Black Pieces\Black Pawn.png')
 blackPawn = pygame.transform.scale(blackPawn, (60, 60))
 blackPawnCaptured = pygame.transform.scale(blackPawn, (45, 45))
 
-blackPictures = [blackRook, blackKnight, blackBishop, blackKing, blackQueen, blackPawn]
-blackPicturesCaptured = [blackRookCaptured, blackKnightCaptured, blackBishopCaptured, blackKingCaptured, blackQueenCaptured, blackPawnCaptured]
+blackPictures = [blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackPawn]
+blackPicturesCaptured = [blackRookCaptured, blackKnightCaptured, blackBishopCaptured, blackQueenCaptured, blackKingCaptured, blackPawnCaptured]
 
 blackPiecesLocation = [(0,0), (1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0),
                        (0,1), (1,1), (2,1), (3,1), (4,1), (5,1), (6,1), (7,1)]
